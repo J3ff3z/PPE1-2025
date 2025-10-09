4b6b44599590b9c2f629faafe129f4fe8cd3c413
Julien-Erwan Truc--Charasch <juer59@hotmail.fr>
2025-10-09 12:04:03 +0200

    Correctif options

--- Exercice1/count.sh
+++ Exercice1/count.sh
@@ -5,19 +5,29 @@ YEAR=0
 TYPE="*"
 
 
+#
+Help(){
+	echo "help"
+}
+
+
 # Get the input options
 
-while getopts ":tac:" option; do
+while getopts "t:a:hc" option; do
 	case $option in
 		t) # Filtre selon le type donne en argument
-			TYPE=$OPTARG
+			TYPE="$OPTARG"
 			;;
 		a) # Ordonne par annee
-			YEAR=$OPTARG
+			YEAR="$OPTARG"
 			if ! [[ $YEAR =~ ^[0-9]+$ ]]; then
 				echo "Error: Invalid Arguments. The year must be a number"
 				exit 1
 			fi
+			;;
+		h) # help
+			Help
+			exit 1
 			;;
 		\?) # Invalid option
 			echo "Error: Invalid option. Please use -h to see the help"
